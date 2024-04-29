9594b23cbcfa5fe990a6b55931592137298f0278
MChurra <mchurra@outlook.pt>
2024-04-29 19:37:58 +0100

    Added the Files

---
+++ GenshinData.js
@@ -0,0 +1,97 @@
+export const genshinStickers = [
+    {
+        emotionTag: ["calm"],
+        image: "/stickers/AyakaCalm",
+        alt: "A Ayaka sticker, calmly drinking some tea",
+    },
+    {
+        emotionTag: ["Confident"],
+        image: "/stickers/BeidouConfident",
+        alt: "A Beidou sticker, confidently laughing",
+    },
+    {
+        emotionTag: ["Confident"],
+        image: "/stickers/EulaConfident",
+        alt: "A Eula sticker, giving you a thumbs up to provide you with confidence",
+    },
+    {
+        emotionTag: ["Confident"],
+        image: "/stickers/JeanConfident",
+        alt: "A Jean sticker, giving you a thumbs up to provide you with confidence",
+    },
+    {
+        emotionTag: ["Brave"],
+        image: "/stickers/KageroumaruBrave",
+        alt: "A Kageroumaru sticker, very eerily and stoic like looking at you, giving you the courage to press on",
+    },
+    {
+        emotionTag: ["Pride"],
+        image: "/stickers/KeqingPride",
+        alt: "A cherfull Keqing sticker, happily looking at you with pride",
+    },
+    {
+        emotionTag: ["Brave"],
+        image: "/stickers/KleeBrave",
+        alt: "A Klee sticker, cheering you on to keep going",
+    },
+    {
+        emotionTag: ["Confident"],
+        image: "/stickers/KujouSaraConfident",
+        alt: "A confident Kujou Sara sticker, giving out orders",
+    },
+    {
+        emotionTag: ["Pride"],
+        image: "/stickers/MonaPride",
+        alt: "A very proud Mona sticker",
+    },
+    {
+        emotionTag: ["Pride"],
+        image: "/stickers/PaimonPride",
+        alt: "A very proud and strong Paimon sticker, beware",
+    },
+    {
+        emotionTag: ["Confident"],
+        image: "/stickers/Raiden ShogunConfident",
+        alt: "A Raiden shogun sticker, sending off a bird",
+    },
+    {
+        emotionTag: ["Brave"],
+        image: "/stickers/SayuBrave",
+        alt: "A battle ready Sayu sticker",
+    },
+    {
+        emotionTag: ["Calm"],
+        image: "/stickers/ThomaCalm",
+        alt: "A calm Thoma sticker, with Kageroumaru sitting on top",
+    },
+    {
+        emotionTag: ["Confident"],
+        image: "/stickers/ThomaConfident",
+        alt: "A confident Thoma sticker",
+    },
+    {
+        emotionTag: ["Brave"],
+        image: "/stickers/XianglingBrave",
+        alt: "A Xiangling sticker cheering you on",
+    },
+    {
+        emotionTag: ["Confident"],
+        image: "/stickers/XinyanConfident",
+        alt: "A confident Xinyan sticker, ready for action",
+    },
+    {
+        emotionTag: ["Calm"],
+        image: "/stickers/YanfeiCalm",
+        alt: "A calm Yanfei sticker, giving you advice",
+    },
+    {
+        emotionTag: ["Confident"],
+        image: "/stickers/YoimiyaConfident",
+        alt: "A confident Yoimiya sticker, cheering you on",
+    },
+    {
+        emotionTag: ["Calm"],
+        image: "/stickers/ZhongliCalm",
+        alt: "A calm Zhongli sticker, drinking tea",
+    },
+]
